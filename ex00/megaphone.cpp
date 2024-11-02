9ee8bf842b43ff95c6c045365f2974cf53aad995
Thelmi <thelmy452@gmail.com>
2024-11-03 00:17:13 +0400

    created the megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main(int ac, char **av)
+{
+	int i = 1;
+	int j = 0;
+	if (ac == 1)
+		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std:: endl;
+	else
+	{
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+				std:: cout << (char)toupper(av[i][j++]);
+			i++;
+		}
+		std:: cout << std:: endl;
+	}
+}
